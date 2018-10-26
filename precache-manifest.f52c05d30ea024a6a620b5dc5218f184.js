51974083431c36f1066412b2ae9d4d3f92757ad0
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2018-10-26 15:34:34 +0100

    Updates

---
+++ precache-manifest.f52c05d30ea024a6a620b5dc5218f184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a5afa49680068d51cd",
+    "url": "/notely/static/js/runtime~main.d2a5afa4.js"
+  },
+  {
+    "revision": "b491d0fd43dc0b608dad",
+    "url": "/notely/static/js/main.b491d0fd.chunk.js"
+  },
+  {
+    "revision": "8a085564c8a1034c443f",
+    "url": "/notely/static/js/1.8a085564.chunk.js"
+  },
+  {
+    "revision": "b491d0fd43dc0b608dad",
+    "url": "/notely/static/css/main.521af0a6.chunk.css"
+  },
+  {
+    "revision": "0acea20596ddf74bc15291c9eaaf1b5b",
+    "url": "/notely/index.html"
+  }
+];
